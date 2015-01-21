770826ea37dbb31c482a10627e24032117f60fbf
Leandro Yabut <arjay.yabut07@gmail.com>
2015-01-21 07:29:30 -0700

    Update AtomScript.js

--- bin/AtomScript.js
+++ bin/AtomScript.js
@@ -12,15 +12,15 @@
  *		AtomScript is an interpreted programming language. The language is translated into JavaScript.
  * 
  */
-window.onload=onLoad;var AtomScript={src:null,include:[],FORMAT:false,consolePath:"AtomScript/console/main.html",startConsole:false};var Console={};var code="";function onLoad(){console.log("%cAtomScript v0.5","color: #0355ff; font-family: arial;");console.log("%c©ZeroSeven Interactive 2015","color: #ff0330; font-family: arial;");if(AtomScript.src!=null&&AtomScript.src.endsWith(".atom")){setScript(AtomScript.src);parseCode();eval(code+"if(main){ if(AtomScript.startConsole)Console.start(); main(); }");}else if(AtomScript.src!=null&&AtomScript.src.startsWith("#")){var id=AtomScript.src.substring(1,AtomScript.src.length);var script=document.getElementById(id);if(script.getAttribute("type")=="AtomScript"){code=script.innerHTML;parseCode();eval(code+"if(main){ if(AtomScript.startConsole)Console.start(); main(); }");}else{console.log("%cMake sure the type of your script tag is 'AtomScript'...","color: #f00;");}}}
+window.onload=onLoad;var AtomScript={src:null,include:[],FORMAT:false,consolePath:"AtomScript/console/main.html",startConsole:false};var Console={};var code="";function onLoad(){console.log("%cAtomScript v0.5","color: #0355ff; font-family: arial;");console.log("%c©ZeroSeven Interactive 2015","color: #ff0330; font-family: arial;");if(AtomScript.src!=null&&AtomScript.src.endsWith(".atom")){setScript(AtomScript.src);parseCode();eval(code+"if(main){ if(AtomScript.startConsole)Console.start(); main(); }");}else if(AtomScript.src!=null&&AtomScript.src.startsWith("#")){var id=AtomScript.src.substring(1,AtomScript.src.length);var script=document.getElementById(id);if(script.getAttribute("type")=="AtomScript"){code=script.innerHTML;parseCode();eval(code+"if(main){ if(AtomScript.startConsole)Console.start(); main(); }");}else{console.log("%cMake sure the type of your script tag is 'AtomScript'...","color: #f00;");}}else if(AtomScript.src==null){var scripts=document.getElementsByTagName("script");var AtomScripts=[];for(var i=0;i<scripts.length;i++){var script=scripts[i];if(script.getAttribute("type")=="AtomScript"||script.getAttribute("type")=="text/AtomScript"){code=script.innerHTML;}}}}
 function parseCode(){includeFiles();if(AtomScript.FORMAT)formatCode();convertVariables();convertMethods();convertObjects();convertObjectProperties();convertNameSpaceSplitters();removeComments();}
 function formatCode(){code=code.replace(/[\n\t\r]/g,"");}
-function removeComments(){code=code.replace(/#[^;]+;/g,"");}
-function convertVariables(){var matches=code.match(/@[^; ]+/g);if(matches!=null)
+function removeComments(){code=code.replace(/^#[^\n]+/g,"");}
+function convertVariables(){var matches=code.match(/\B@\w+/g);if(matches!=null)
 for(var i=0;i<matches.length;i++){code=code.replace(matches[i].substring(0,1),"var ");}}
 function convertMethods(){var matches=code.match(/\$[^; ]+/g);if(matches!=null)
 for(var i=0;i<matches.length;i++){code=code.replace(matches[i].substring(0,1),"function ");}}
-function convertObjects(){var matches=code.match(/\*[^;0-9 ]+/g);if(matches!=null)
+function convertObjects(){var matches=code.match(/^\B\*[^;0-9 ]+/g);if(matches!=null)
 for(var i=0;i<matches.length;i++){code=code.replace(matches[i].substring(0,1),"new ");}}
 function convertObjectProperties(){code=code.replace(/this ->|this->|this-> |this -> /g,"this.");}
 function convertNameSpaceSplitters(){var matches=code.match(/::/g);if(matches!=null)
@@ -41,6 +41,6 @@ i.getText=function(){return i.innerText;}
 return i;}
 function createDiv(){var i=Doc.createElement("div");return i;}
 function append(element,parent){if(parent==null)Doc.body.appendChild(element);else parent.appendChild(element);}
-function get(id){if(id.startsWith("#")){return document.getElementById(id.substring(1,id.length));}}
+function get(id,root){if(root==null)root=document;if(id.startsWith("#")){return root.getElementById(id.substring(1,id.length));}else if(id.startsWith(".")){return root.getElementsByClassName(id.substring(1,id.length));}else if(id.startsWith("<")&&id.endsWith(">")){return root.getElementsByTagName(id.substring(1,id.length-1));}else if(id.startsWith("^")){return root.getElementsByName(id.substring(1,id.length));}else{return id;}}
 Console.start=function(){Console.Window=window.open(AtomScript.consolePath,"AtomScriptConsole","width=650,height=440,menubar=no,statusbar=no,location=no");Console.Document=Console.Window.document;Console.inputElement=Console.Document.getElementById("input");Console.displayElement=Console.Document.getElementById("display");Console.out("Welcome to the AtomScript Console!");}
 Console.out=function(t){var text=createText(t);text.style.display="block";append(text,Console.displayElement);}
